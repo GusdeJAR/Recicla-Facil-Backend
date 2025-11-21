167fd7d904b1ea2b90e4057a8b0fd5737fe07047
GusdeJAR <apariciorgustavo@gmail.com>
2025-11-21 15:39:01 -0700

    Resend

--- api/services/mailService.js
+++ api/services/mailService.js
@@ -1,10 +1,10 @@
 const { Resend } = require('resend');
 
-const resend = new Resend('re_KtfoQvjE_BrYdGJ8YAgznKtV2SiCGExsE');
+const resend = new Resend('re_Uoqafv2M_6rAAJX2MLYARh7u6ywaLxiir');
 
 async function enviarCorreo(destinatario, asunto, texto) {
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'apariciorgustavo@gmail.com',
         to: destinatario,
         subject: asunto,
         html: `<p>${texto}</p>`
