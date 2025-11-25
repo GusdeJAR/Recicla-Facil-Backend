fdcf29200bcae654a973100fa18cc5822bc08fad
jolumart20 <jolumartinezro@ittepic.edu.mx>
2025-11-24 19:10:28 -0700

    index

--- api/index.js
+++ api/index.js
@@ -1,31 +1,18 @@
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/router'); // La ruta ahora es directa
 const conectarDB = require('./config/db');   // La ruta ahora es directa
-<<<<<<< HEAD
-const host = '0.0.0.0';
-const port=3000;
 
-=======
-const imageRoutes = require('./routes/images');
->>>>>>> 7051052bbdddd8bae97feedece29c57c14129ca6
 const app = express();
+
 conectarDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/images', imageRoutes);
+// ¡CRUCIAL! Ahora que el index está en la raíz, montamos el router en /api
 app.use('/api', router);
-<<<<<<< HEAD
-app.listen(port, host, () => {
-  console.log(`🚀 Servidor listo para recibir conexiones.`);
-  console.log(`   - Localmente en: http://localhost:${port}`);
-});
 
-
-=======
->>>>>>> 7051052bbdddd8bae97feedece29c57c14129ca6
 // Exportamos la app para Vercel
 module.exports = app;
